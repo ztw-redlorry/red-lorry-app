1c8f377d7c1af89c3e3d583539181c7219af6938
Bart <bart.mikolajczuk@gmail.com>
2019-03-17 19:42:25 +0100

    Order & Transport elements

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.scss';
+import OrderTile from "./components/OrderTile/OrderTile";
+import TransportTile from "./components/TransportTile/TransportTile";
+import OrdersColumn from "./components/OrdersColumn/OrdersColumn";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and asdsave to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className={'container'}>
+            <OrdersColumn/>
+            {/*<TransportTile transportNumber={5} transportRoute={['Katowice', 'Bytom', 'Pyrzowice']} handledOrders={['Zamówienie 1', 'Zamówienie 2', 'Zamówienie 3']}/>*/}
+            </div>
+        );
+    }
 }
 
 export default App;
