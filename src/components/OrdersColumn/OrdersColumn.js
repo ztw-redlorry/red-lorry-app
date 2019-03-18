d4b1f1e969cf68bd64348b9d523fdd24345d77e7
Aleksander <alek.paliszewski@gmail.com>
2019-03-18 14:50:05 +0100

    css

--- src/components/OrdersColumn/OrdersColumn.js
+++ src/components/OrdersColumn/OrdersColumn.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import {Image} from 'react-bootstrap';
 import './OrdersColumn.scss';
 import OrderTile from "../OrderTile/OrderTile";
 import plus from '../../plus.png';
+import Button from "react-bootstrap/Button";
+
 
 class OrdersColumn extends Component{
+
     render() {
         return (
             <div className={'ordersColumn'}>
@@ -15,8 +17,10 @@ class OrdersColumn extends Component{
                 <OrderTile orderNumber={3} pointFrom={'Katowice'} pointTo={'Bytom'} amount={100}/>
                 <OrderTile orderNumber={3} pointFrom={'Katowice'} pointTo={'Bytom'} amount={100}/>
                 <OrderTile orderNumber={3} pointFrom={'Katowice'} pointTo={'Bytom'} amount={100}/>
-                <OrderTile orderNumber={3} pointFrom={'Katowice'} pointTo={'Bytom'} amount={100}/>
-                <OrderTile orderNumber={3} pointFrom={'Katowice'} pointTo={'Bytom'} amount={100}/>
+                <Button onclick={props.onAddChild.bind(this)}></Button>
+                <div id="children-pane">
+                    {props.children}
+                </div>
                 <img className={'addOrder' } src={plus}/>
             </div>
         )
