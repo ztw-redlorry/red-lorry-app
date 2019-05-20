cbe9f311d014ef136cf5a11e049ff813a4e3399c
snufkin <strzalkafilip@gmail.com>
2019-05-20 14:09:31 +0200

    Enhanced TransportCapacity component.

--- Frontend/src/components/TransportCapacity/TransportCapacity.js
+++ Frontend/src/components/TransportCapacity/TransportCapacity.js
@@ -6,12 +6,19 @@ import LogisticScreen from "../PanelScreen/PanelScreen";
 const TransportCapacity = (props) => (
     <div className={styles.capacityScreenWrapper}>
         <div className={styles.progressBarWrapper}> {
-            props.routePointsArray.map((point) => (
+            props.routePointsArray.map((point) => (point.load==0?(
+                    <div className={styles.routePartEnd}>
+                        <div>{point.title}</div>
+                        <div style={{height: '80px'}}></div>
+                        <div>{point.load}</div> 
+                    </div>
+                ):(
                     <div className={styles.routePart}>
                         <div>{point.title}</div>
-                        <ProgressBar style={{height: '20px'}} now={point.load / props.maxLoad * 100}/>
+                        <ProgressBar style={{height: '60px'}} now={point.load / props.maxLoad * 100}/>
+                        <div>{point.load}</div>
                     </div>
-                )
+                ))
             )
         }
         </div>
